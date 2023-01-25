6a70c4b9da1fcea868cbe0ced0a9bb867d0d270b
mendri <marcalmendrighi@gmail.com>
2023-01-25 16:38:18 -0300

    refactor: transformando o Login router em classe

--- backend/src/Controllers/LoginController.ts
+++ backend/src/Controllers/LoginController.ts
@@ -2,23 +2,28 @@ import { NextFunction, Request, Response } from "express";
 import LoginService from "../Services/LoginService";
 
 class LoginController {
+    private req: Request;
+    private res: Response;
+    private next: NextFunction;
     private service: LoginService;
 
-    constructor() {
-        this.service = new LoginService();
-    }
-
-    public async login(
-        _req: Request,
+    constructor(
+        req: Request,
         res: Response,
         next: NextFunction
     ) {
+        this.req = req;
+        this.res = res;
+        this.next = next;
+        this.service = new LoginService();
+    }
+
+    public async login() {
         try {
             const response = await this.service.handleLoginService();
-            return res.status(200).json(response);
+            return this.res.status(200).json(response);
         } catch(e) {
             console.log(e);
-            next();
         }
     }
 }
--- backend/src/Routes/LoginRouter.ts
+++ backend/src/Routes/LoginRouter.ts
@@ -3,16 +3,14 @@ import LoginController from "../Controllers/LoginController";
 
 class LoginRouter {
     public router: Router;
-    private controller: LoginController;
 
     constructor() {
         this.router = Router();
-        this.controller = new LoginController();
         this.setupRoutes();
     }
 
     private async setupRoutes() {
-        this.router.get("/", (req, res, next) => this.controller.login(req, res, next));
+        this.router.get("/", (req, res, next) => new LoginController(req, res, next).login());
     }
 }
 
